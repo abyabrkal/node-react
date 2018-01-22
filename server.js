5d491949cb38f2f72dc50a275e2faad048eb6092
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2018-01-22 13:08:25 +0400

    HTTP server start

---
+++ server.js
@@ -0,0 +1,13 @@
+//import config from './config';
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.write('Hello HTTP!\n');
+
+    setTimeout(() => {
+        res.write('I am a stream!\n');
+        res.end();
+    }, 3000);
+});
+
+server.listen(8080);
