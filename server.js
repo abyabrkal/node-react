73d96239672b21222f875d6bb544f2d9964d938c
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2018-01-23 12:38:35 +0400

    Express Server
    Server ON and Routing ON

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-//import config from './config';
-import http from 'http';
+import config from './config';
+import apiRouter from './api'
 
-const server = http.createServer((req, res) => {
-    res.write('Hello HTTP!\n');
+import express from 'express';
+const server = express();
 
-    setTimeout(() => {
-        res.write('I am a stream!\n');
-        res.end();
-    }, 3000);
+// 2. In addition to port listening, 
+//    it also does server-side rotuing 
+server.get('/', (req, res) => {
+    //use res.send() for sending simple strings
+    res.send('Hello Express');
 });
 
-server.listen(8080);
+// Middleware for serving public files
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+// 1. Express listening to single port for any user requests
+server. listen(config.port, () => {
+    console.info("Express listening on port", config.port);
+});
