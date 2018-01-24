152876498e65d54be7ceddf0a7975e5b8406f257
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2018-01-24 12:02:14 +0400

    React components and JSX
    React components and JSX
    Webpack+

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { SSL_OP_PKCS1_CHECK_2 } from 'constants';
+
+ReactDOM.render(    
+    <h2 className="text-center">
+        Hello React from JSX!!
+    </h2>,
+    document.getElementById('root')
+);
