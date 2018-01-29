a9f509402dd6c3a6d7dca64e3625b1e18e9a931c
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2018-01-29 11:26:37 +0400

    Component LifeCycle
    component  Mounting/Unmounting

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,15 @@ class App extends React.Component {
 	state = {
 		pageHeader: 'Naming Contests'
 	};
+	componentDidMount(){
+		//This code ensures that React DOM is mounted
+		//If any external API(via ajax call) is fetched 
+		//we will use the ajax call here, to ensure DOM'smount is completed.
+		// For Timers & Listeners
+	}
+	componentWillUnmount(){
+		// Clean Timers and Listeners
+	}
 	render() {
 		return (
 	        <div className="App">
