2a187dfdb30f1ca4b45d670d63e65d9653a13416
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2018-01-30 11:32:41 +0400

    ERROR: continues
    Eror 1: The above error occurred in the <ContestPreview> component:
    Consider adding an error boundary to your tree to customize error
    handling behavior.
    Visit https://fb.me/react-error-boundaries to learn more about error
    boundaries.
    
    Error 2: ContestPreview(...):
    Nothing was returned from render. This usually means a return statement
    is missing. Or, to render nothing, return null.

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,37 @@
 import React from 'react';
 import Header from './Header';
+import PropTypes from 'prop-types'; 
 import ContestPreview from './ContestPreview';
 
+
+/* IMSIDE DEEP DIV
+
+          {this.props.contests.map(contest =>
+            <ContestPreview {...contest} />
+		  )}
+*/
+
+
 class App extends React.Component {
-	state = {
-		pageHeader: 'Naming Contests'
-	};
-	componentDidMount(){
-		//This code ensures that React DOM is mounted
-		//If any external API(via ajax call) is fetched 
-		//we will use the ajax call here, to ensure DOM'smount is completed.
-		// For Timers & Listeners
-	}
-	componentWillUnmount(){
-		// Clean Timers and Listeners
-	}
-	render() {
-		return (
-	        <div className="App">
-	            <Header message={this.state.pageHeader} />
-	            <div>
-					{this.props.contests.map(contest =>
-						<ContestPreview {...contest}/>
-					)}
-				</div> 
-	        </div>
-	    );
-	}
-};
+  state = {
+    pageHeader: 'Naming Contests'
+  };
+  componentDidMount() {
+    // timers, listeners
+  }
+  componentWillUnmount() {
+    // clean timers, listeners
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+			<ContestPreview {...this.props.contests[0]} />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types'; 
 
 import data from './testData';
-
-
 import App from './components/App';
 
-ReactDOM.render(    
-    <App contests={data.contests}/>,
-    document.getElementById('root')
+ReactDOM.render(
+  <App contests={data.contests} />,
+  document.getElementById('root')
 );
